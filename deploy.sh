6007e12e07d806599dc616f270fad3f6ad5dc6f8
Joshua <jbautista396@gmail.com>
2020-04-21 18:07:57 -0400

    Fix deploy.sh bug

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,11 @@ GIT_VERSION=$(git describe --always --abbrev --tags --long) # Git hash and tags
 
 # Build and tag image in root directory
 docker-compose build
-docker images
 docker tag advicetracker_client:latest advicetracker:client
 docker tag advicetracker_server:latest advicetracker:server
 
 # Log in to Docker Hub and push
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker images
-docker push kthisisjosh/advicetracker:client
-docker push kthisisjosh/advicetracker:server
+docker push advicetracker:client
+docker push advicetracker:server
 echo "Successfully deployed client and server to Docker Hub!"
