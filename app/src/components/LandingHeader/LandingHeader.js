c6a4104dacbc061fcfc82655c61611d384e30f7d
Joshua <jbautista396@gmail.com>
2020-04-22 21:28:08 -0400

    Landing Header fully responsive

--- app/src/components/LandingHeader/LandingHeader.js
+++ app/src/components/LandingHeader/LandingHeader.js
@@ -4,17 +4,37 @@ import Grid from "@material-ui/core/Grid"
 import LoginButton from "../../components/LoginButtons/LoginButton"
 import RegisterButton from "../../components/LoginButtons/RegisterButton"
 import { Link } from "react-router-dom"
+import { slide as Menu } from "react-burger-menu"
 
 const Header = () => {
-    return (
-        <header style={{ backgroundColor: "#FFFFFF", margin: "0", width: "100vw"}}>
-            <Grid container direction="row" alignItems="center" style={{ height: "60px", alignContent: "center" }}>
-                    <img alt="logo" src="Logo.svg" x="50" y="18" height="40" width="40" style={{marginLeft: "10vw"}}/>
+    const showSettings = (event) => {
+        event.preventDefault()
+    }
 
-                <Typography variant="h4" justify="left" style={{ marginTop: "0.5vh", marginBottom: "1vh", marginLeft: "0.5vw", color: "black" }}>
-                    Advice Tracker
-                </Typography>
-                <Grid item className="navbar-grid" style={{ marginLeft: "25vw" }}>
+    return (
+        <header style={{ backgroundColor: "#FFFFFF", margin: "0", width: "100vw" }}>
+            <Grid container direction="row" alignItems="center" style={{ height: "60px", alignContent: "center" }}>
+                <Grid item style={{marginLeft: "auto"}}>
+                    <img alt="logo" src="Logo.svg" x="50" y="18" height="40" width="40" style={{ marginLeft: "10vw" }} />
+                </Grid>
+                <Grid item style={{marginRight: "auto"}}>
+                    <Typography variant="h4" justify="left" style={{ marginTop: "0.5vh", marginBottom: "1vh", marginLeft: "0.5vw", color: "black" }}>
+                        Advice Tracker
+                    </Typography>
+                </Grid>
+
+                <Grid item style={{ marginLeft: "auto", marginRight: "5vw"}}>
+                    <Menu right noOverlay>
+                        <Link to="/browse" style={{ margin: "0 0 0 5vw", textDecoration: "none", color: "black" }}>
+                            <Typography variant="button">Browse</Typography>
+                        </Link>
+                        <Link to="/login" style={{ margin: "0 0 0 5vw", textDecoration: "none", color: "black" }}>
+                            <Typography variant="button">Login/Register</Typography>
+                        </Link>
+                    </Menu>
+                </Grid>
+
+                <Grid item className="navbar-grid" style={{ marginLeft: "auto" }}>
                     <Link to="/browse" style={{ margin: "0 0 0 5vw", textDecoration: "none", color: "black" }}>
                         <Typography variant="button">Browse</Typography>
                     </Link>
