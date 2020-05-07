093c92bd7aa91849c587e8b3839e911102fa422b
Joshua <jbautista396@gmail.com>
2020-05-07 14:50:30 -0400

    Fix AddNewButton bug

--- app/src/components/DashboardPage/Inbox/AddNewButton.js
+++ app/src/components/DashboardPage/Inbox/AddNewButton.js
@@ -4,7 +4,7 @@ import AddIcon from '@material-ui/icons/Add';
 
 const AddNewButton = (props) => {
     return (
-        <Button variant="contained" onClick={props.handleAddClick} style={{ backgroundColor: "F2994A" }} startIcon={<AddIcon />}>
+        <Button variant="contained" onClick={props.handleAddClick} style={{ backgroundColor: "#F2994A" }} startIcon={<AddIcon />}>
             Add New
         </Button>
     )
