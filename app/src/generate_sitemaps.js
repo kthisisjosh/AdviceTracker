f638c642b9c5f15d709d631618d9b5bcbb426f9a
Joshua <jbautista396@gmail.com>
2020-05-10 12:38:56 -0400

    Add sitemap generator script

---
+++ app/src/generate_sitemaps.js
@@ -0,0 +1,27 @@
+const algoliaSitemap = require("algolia-sitemap")
+
+try {
+    const algoliaConfig = {
+        appId: process.env.REACT_APP_ALGOLIA_APP_ID,
+        apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
+        indexName: "Posts",
+    }
+
+    const hitToParams = ({ permalink }) => ({ loc: permalink })
+
+    algoliaSitemap({
+        algoliaConfig,
+        sitemapLoc: "https://advicetracker.life/sitemaps",
+        outputFolder: "sitemaps",
+        hitToParams,
+    })
+} catch (error) {
+    console.log(error)
+}
+
+Promise.all([
+    fetch("http://www.google.com/webmasters/sitemaps/ping?sitemap=http://example.com/sitemap.xml"),
+    fetch("http://www.bing.com/webmaster/ping.aspx?siteMap=http://example.com/sitemap.xml"),
+]).then(() => {
+    console.log("Done")
+})
